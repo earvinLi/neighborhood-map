84a8eeb09e121dc2dc4640c16b2753f6de28042e
Earvin Li <earvin.tli@gmail.com>
2017-11-26 18:00:36 -0800

    add and bind ViewMode

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 let map;
 
+const ViewModel = () => {
+
+};
+
+let map;
 initMap = () => {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 37.7749, lng: -122.4194},
@@ -8,4 +13,5 @@ initMap = () => {
 };
 handleError = () => {
 	alert(`Your map can't be loaded correctly. Please check the console for technical details and solutions.`);
+  ko.applyBindings(new ViewModel());
 };
